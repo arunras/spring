83ba83b27bc6d00906fc595291a14454e742cf8b
Arun <prith000@citymail.cuny.edu>
2018-01-29 03:27:00 -0500

    first commit

---
+++ jwd/src/main/java/core/jwd/servlets/MenuSearchServlet.java
@@ -0,0 +1,41 @@
+package core.jwd.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import core.jwd.data.MenuDataService;
+import core.jwd.domain.MenuItem;
+
+public class MenuSearchServlet extends HttpServlet {
+  public void service (HttpServletRequest request, HttpServletResponse response) throws IOException {
+    PrintWriter out = response.getWriter();
+    response.setContentType("text/html");
+
+    String searchTerm = request.getParameter("searchTerm");
+
+    MenuDataService menuDataService = new MenuDataService();
+    List<MenuItem> menuItems = menuDataService.find(searchTerm);
+
+    if (menuItems.size() > 0) {
+      out.println("<html><body><h1>Ricky's Restaurant</h1>");
+      out.println("<h2>Dishes containing " + searchTerm + "</h2><ul>");
+      for (MenuItem menuItem : menuItems) {
+        out.println("<li>" + menuItem + " " + menuItem.getDescription() + "</li>");
+      }
+      out.println("</body></html");
+    } else {
+      out.println("<html><body><h1>Ricky's Restaurant</h1>");
+      out.println("<p>I'm sorry, there are no dishes containing " + searchTerm);
+      out.println("</body></html");
+    }
+
+
+    out.close();
+  }
+}
+
---
+++ jwd/src/main/java/core/jwd/servlets/ViewMenuServlet.java
@@ -0,0 +1,34 @@
+package core.jwd.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import core.jwd.data.MenuDataService;
+import core.jwd.domain.MenuItem;
+
+public class ViewMenuServlet extends HttpServlet {
+  public void service (HttpServletRequest request, HttpServletResponse response) throws IOException {
+    PrintWriter out = response.getWriter();
+    response.setContentType("text/html");
+
+    MenuDataService menuDataService = new MenuDataService();
+    List<MenuItem> menuItems = menuDataService.getFullMenu();
+
+    out.println("<html><body><h1>Ricky's Restaurant</h1>");
+    out.println("<h2>Menu</h2>");
+    for (MenuItem menuItem : menuItems) {
+      out.println("<li>" + menuItem + "</li>");
+    }
+    out.println("</body></html>");
+    out.close();
+  }
+}
+
+
+
